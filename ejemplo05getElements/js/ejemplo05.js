654f5db4e08d957078f5e0cbd45ec2b96ed38d79
Rubén Gómez García <rgomez@pronoide.es>
2019-04-10 08:29:46 +0000

    Ejemplo 04 incompleto

--- ejemplo05getElements/js/ejemplo05.js
+++ ejemplo05getElements/js/ejemplo05.js
@@ -1,7 +1,20 @@
 function inicializar(){
-    var elemH1 = document.createElement('h1');
-    var textH1 = document.createTextNode("Un titulo");
-    elemH1.appendChild(textH1);
-    var body = document.body;
-    body.appendChild(elemH1);
+    var parrafo1 = 
+    document.getElementById('parrafo1');
+    console.log(parrafo1);
+    var parrafo2 =
+    document.querySelector("body p")
+    console.log(parrafo2);
+    var titulos = document.getElementsByClassName("titulo")
+    console.log(titulos);
+    var listItems = document.getElementsByTagName("li");
+    console.log(listItems)
+    var inputs = document.getElementsByName("input");
+    console.log(inputs)
+    var parrafos = document.querySelectorAll("body p");
+    console.log("Primero : ",parrafos.item(0));
+    console.log("Segundo : ",parrafos[1]);
+    for(var i = 0;i < parrafos.length; i++){
+        console.log(parrafos[i].firstChild.textContent);
+    }
 }
